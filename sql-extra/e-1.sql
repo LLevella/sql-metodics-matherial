2b1b2a1e6f24f75fe558312fea80ce491b51dd63
Irina <38900844+LLevella@users.noreply.github.com>
2019-04-19 10:27:21 +0700

    Update e-1.sql

--- sql-extra/e-1.sql
+++ sql-extra/e-1.sql
@@ -11,6 +11,6 @@ alter table public.shipper
 
 alter table public.supply
 	add primary key (sid, pid, jid),
-	add foreign key (sid) REFERENCES public.shipper (id) on delete cascade,
-	add foreign key (jid) REFERENCES public.job (id) on delete restrict,
-	add foreign key (pid) REFERENCES public.products (id) on delete restrict;
+	add foreign key (sid) references public.shipper (id) on delete cascade,
+	add foreign key (jid) references public.job (id) on delete restrict,
+	add foreign key (pid) references public.products (id) on delete restrict;
